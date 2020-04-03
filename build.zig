dc70acd83873b456734b0b260e79af7d1195ceef
Spencer Peterson <spencerjpeterson@gmail.com>
2020-04-03 15:01:00 -0700

    build binary named cat

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("cat-zig", "src/main.zig");
+    const exe = b.addExecutable("cat", "src/main.zig");
     exe.setBuildMode(mode);
     exe.install();
 
